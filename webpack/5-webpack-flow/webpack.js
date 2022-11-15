ad4fda165d07a80899bcd52bcb6594e8b15da3fd
褚鹏飞 <540788769@qq.com>
2022-11-15 08:37:08 +0800

    feat: webpack 工作流

--- webpack/5-webpack-flow/webpack.js
+++ webpack/5-webpack-flow/webpack.js
@@ -7,9 +7,17 @@ function webpack(options) {
     shellConfig[key.slice(2)] = value
     return shellConfig
   }, {})
-
+  // 2 用上一步得到的参数初始化 `Compiler` 对象
   let finalConfig = { ...options, ...shellConfig }
   let compiler = new Compiler(finalConfig)
+
+  // 3 加载所有配置的插件
+  const { plugins } = finalOptions
+  // 每个插件都有一个 apply 方法，执行这个方法的时候，将compiler传递进去。
+  for (let plugin of plugins) {
+    plugin.apply(compiler)
+  }
+  // 最终返回compiler对象
   return compiler
 }
 
