d599252bf818c3ee8a81a3f3895e97c2d1783825
褚鹏飞 <540788769@qq.com>
2022-12-23 14:33:30 +0800

    feat: rollup

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,15 +1,25 @@
 import babel from "@rollup/plugin-babel"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+
 export default {
   input: "src/main.js",
   output: {
     file: "dist/bundle.cjs.js", //输出文件的路径和名称
-    format: "cjs", //五种输出格式：amd/es6/iife/umd/cjs
-    name: "rollup-bundleName", //当format为iife和umd时必须提供，将作为全局变量挂在window下
+    format: "iife", //五种输出格式：amd/es6/iife/umd/cjs
+    name: "bundleName", //当format为iife和umd时必须提供，将作为全局变量挂在window下
+    global: {
+      lodash: "_",
+      jquery: "$",
+    },
   },
+  external: ["lodash", "jquery"],
   plugins: [
     babel({
-      babelHelpers: 'bundled', // 这个配置babel希望被显示的配置上去
+      babelHelpers: "bundled", // 这个配置babel希望被显示的配置上去
       exclude: "node_modules/**",
     }),
+    resolve(),
+    commonjs(),
   ],
 }
--- rollup/src/main.js
+++ rollup/src/main.js
@@ -2,8 +2,17 @@
 // console.log("hello rollup");
 
 // 使用新的语法，用babel来转义
-let sum = (a, b) => {
-  return a + b
-}
-let result = sum(1, 2)
-console.log(result)
+// let sum = (a, b) => {
+//   return a + b
+// }
+// let result = sum(1, 2)
+// console.log(result)
+
+// import _ from "lodash"
+// console.log(_);
+
+import _ from 'lodash';
+import $ from 'jquery';
+console.log(_.concat([1,2,3],4,5));
+console.log($);
+export default 'main';
