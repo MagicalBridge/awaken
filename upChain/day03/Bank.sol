9dfdf1e438bbae4e08f9fde7acb5db8ba4835c1f
chupengfei <450793585@qq.com>
2024-07-04 17:39:57 +0800

    feat: replace the comment to english

--- upChain/day03/Bank.sol
+++ upChain/day03/Bank.sol
@@ -2,74 +2,74 @@
 pragma solidity ^0.8.24;
 
 contract Bank {
-    // 状态变量owner, 是合约的所有者
+    // State variable owner, the owner of the contract
     address public owner;
-    // 声明一个映射，地址类型 -> 账户的余额，合约内部访问
+    // Declare a mapping, address type -> account balance, internal access within the contract
     mapping (address => uint256) private balances;
-    // 一个数组，存放存款最多的用户
+    // An array to store the users with the highest deposits
     address[] public topDepositors;
 
-    // 声明一个事件，在取款的时候触发
+    // Declare an event triggered on withdrawal
     event Withdrawal(address indexed to, uint256 amount);
-    // 声明一个事件，存钱的时候触发
+    // Declare an event triggered on deposit
     event Deposit(address indexed from, uint256 amount);
 
     constructor() {
-        // 在构造函数中，将owner设置为msg.sender, 将部署的人设置为合约所有者
+        // In the constructor, set the owner to msg.sender, setting the deployer as the contract owner
         owner = msg.sender;
     }
 
-    // 权限控制，只有合约所有者才可以执行某些操作
+    // Access control, only the contract owner can execute certain operations
     modifier onlyOwner () {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
 
-    // 声明receive回调函数，允许合约接收主币
+    // Declare receive callback function, allowing the contract to receive native currency
     receive() external payable {
-        // 有人向合约发送以太币的时候，直接调用deposit函数
+        // When someone sends Ether to the contract, directly call the deposit function
         deposit();
     }
 
-    // 存款函数，允许外部账户向合约存入相应的以太币
+    // Deposit function, allowing external accounts to deposit Ether into the contract
     function deposit() public payable {
-        // 用户发送的主币数量必须大于0;
+        // The amount of native currency sent by the user must be greater than 0
         require(msg.value > 0, "Deposit amount must be greater than zero");
-        // 更新每个地址的余额
+        // Update the balance of each address
         balances[msg.sender] += msg.value;
-        // 更新排行榜
+        // Update the leaderboard
         updateTopDepositors(msg.sender);
-        // 触发事件
+        // Trigger the event
         emit Deposit(msg.sender, msg.value);
     }
 
-    // 取款函数，只允许合约拥有者提款
+    // Withdrawal function, only allows the contract owner to withdraw
     function withdraw(uint256 amount) public onlyOwner {
-        // 获取当前Bank合约的余额
+        // Get the current balance of the Bank contract
         uint256 balance = address(this).balance;
-        // 如果余额不足，不再支持取款
+        // If the balance is insufficient, withdrawal is not supported
         require(balance > 0, "Contract balance is zero");
-        // 提取的金额应该小于目前这个合约中存在的钱
+        // The amount to be withdrawn should be less than the current balance in the contract
         require(amount <= balance, "Insufficient contract balance");
-        // 合约所有者，可以取款，单位是wei, 注意进制的转换。
+        // The contract owner can withdraw, the unit is wei, note the conversion of units.
         payable(owner).transfer(amount);
-        // 触发取款事件
+        // Trigger the withdrawal event
         emit Withdrawal(owner, amount);
     }
 
-    // 不断更新排名
+    // Continuously update the ranking
     function updateTopDepositors(address depositor) internal {
-        // 设置一个标识，避免重复添加
+        // Set a flag to avoid duplication
         bool exists = false;
 
-        // 遍历数组,如果当前地址已经存在于topDepositors中，跳出循环
+        // Traverse the array, if the current address already exists in topDepositors, exit the loop
         for (uint256 i = 0; i < topDepositors.length; i++) {
             if (topDepositors[i] == depositor) {
                 exists = true;
                 break;
             }
         }
-        // 如果不存在，将当前的地址放入数组中
+        // If it does not exist, add the current address to the array
         if (!exists) {
             topDepositors.push(depositor);
         }
@@ -84,18 +84,18 @@ contract Bank {
             }
         }
 
-        // 数组长度大于3之后，只保存前三个。
+        // After the array length exceeds 3, only keep the top three.
         if (topDepositors.length > 3) {
             topDepositors.pop();
         }
     }
 
-    // 根据指定地址查看账户余额
+    // View the account balance based on the specified address
     function getBalance(address addr) public view returns(uint256) {
         return balances[addr];
     }
 
-    // 返回存钱排行榜
+    // Return the deposit leaderboard
     function getTopDepositors() public view returns (address[] memory) {
         return topDepositors;
     }
